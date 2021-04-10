5b19948365b3487d898254aa281f50e8850140bd
Erika M <emeraz15@gmail.com>
2021-04-10 15:36:49 -0500

    Added current icon from API

--- src/index.js
+++ src/index.js
@@ -78,6 +78,12 @@ function displayTemp(response) {
   let wind = Math.round(response.data.wind.speed);
   let windSpeed = document.querySelector(".wind");
   windSpeed.innerHTML = `${wind}mph`;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function displayCurrentcity(response) {
